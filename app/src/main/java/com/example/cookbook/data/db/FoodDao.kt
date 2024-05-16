62f0d24454e323fc549f52ed49d22049e7aae2c3
aminsaberi52 <118849395+aminsaberi52@users.noreply.github.com>
2024-05-16 14:13:41 +0330

    Update FoodDao.kt

--- app/src/main/java/com/example/cookbook/data/db/FoodDao.kt
+++ app/src/main/java/com/example/cookbook/data/db/FoodDao.kt
@@ -1,4 +1,18 @@
 package com.example.cookbook.data.db
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cookbook.data.model.Food
+
+@Dao
 interface FoodDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllFoods(foods: List<Food>)
+
+    @Query("SELECT * FROM food")
+    fun getAllFoods(): List<Food>
+
 }
