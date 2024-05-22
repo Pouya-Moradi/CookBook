c7715374efc9e69fbd5fbaccc8675ffe52a7a4d2
Pouya_Moradi <pouyaamoradii13800@gmail.com>
2024-05-22 21:52:09 +0330

    refining the FoodListScreen.kt

--- app/src/main/java/com/example/cookbook/ui/screen/FoodListScreen.kt
+++ app/src/main/java/com/example/cookbook/ui/screen/FoodListScreen.kt
@@ -1,67 +1,40 @@
-package com.example.cookbook.ui.screen
-
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.example.cookbook.data.FoodDataProvider
-import com.example.cookbook.data.model.Food
-
+// Composable function for displaying the food list screen
 @Composable
 fun FoodListScreen(
-    modifier: Modifier = Modifier,
-    foodCategoryId: Int,
-    foods: List<Food>,
-    onFoodItemClick: (Int) -> Unit
+    modifier: Modifier = Modifier, // Modifier for styling
+    foodCategoryId: Int, // ID of the food category
+    foods: List<Food>, // List of foods in the category
+    onFoodItemClick: (Int) -> Unit // Callback for food item click
 ) {
-    val foodCategory = FoodDataProvider.foodCategories.find { foodCategory -> foodCategory.id == foodCategoryId }
-    Surface(modifier = modifier
-        .fillMaxSize()
-        .background(color = Color(0xFFF9E4C8)),
-        color = Color(0xFFF9E4C8)) {
+    // Find the food category by ID
+    val foodCategory = FoodDataProvider.foodCategories.find { it.id == foodCategoryId }
+
+    // Surface for holding the entire screen content
+    Surface(
+        modifier = modifier.fillMaxSize().background(color = Color(0xFFF9E4C8)), // Styling and background color
+        color = Color(0xFFF9E4C8) // Background color
+    ) {
+        // Column for arranging content vertically
         Column(
-            Modifier.padding(15.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(15.dp), // Padding for content
+            horizontalAlignment = Alignment.CenterHorizontally // Center align content horizontally
         ) {
+            // Text displaying the name of the food category
             Text(
-                modifier = modifier.padding(50.dp),
-                text = foodCategory!!.name,
-                style = MaterialTheme.typography.displaySmall,
-                textAlign = TextAlign.Center // تراز کردن متن به وسط
+                modifier = Modifier.padding(50.dp), // Padding for the text
+                text = foodCategory!!.name, // Name of the food category
+                style = MaterialTheme.typography.displaySmall, // Typography style
+                textAlign = TextAlign.Center // Text alignment
             )
 
-
-            //Spacer(modifier = Modifier.height(15.dp)) // فاصله بین تابع Text و LazyColumn
-
+            // Lazy column for displaying food items
             LazyColumn {
                 items(foods, key = { food -> food.id }) { food ->
+                    // Composable function for individual food item
                     FoodListItem(
-                        food = food,
-                        modifier = Modifier.padding(horizontal = 8.dp),
-                        onFoodItemClick
+                        food = food, // Food item
+                        modifier = Modifier.padding(horizontal = 8.dp), // Padding for the item
+                        onFoodItemClick // Callback for item click
                     )
                 }
             }
@@ -69,41 +42,44 @@ fun FoodListScreen(
     }
 }
 
+// Composable function for displaying individual food item
 @Composable
 private fun FoodListItem(
-    food: Food,
-    modifier: Modifier = Modifier,
-    onFoodItemClick: (Int) -> Unit
+    food: Food, // Food item
+    modifier: Modifier = Modifier, // Modifier for styling
+    onFoodItemClick: (Int) -> Unit // Callback for item click
 ) {
+    // Surface for holding the item content
     Surface(
         modifier = modifier
             .fillMaxWidth()
-            .height(120.dp)
-
-            .padding(vertical = 4.dp, horizontal = 12.dp)
-            .clickable {
-                onFoodItemClick(food.id)
-            },
-        shape = RoundedCornerShape(4.dp),
-        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
-        color = Color(0xFFF9CF93)
+            .height(120.dp) // Height of the item
+            .padding(vertical = 4.dp, horizontal = 12.dp) // Padding for the item
+            .clickable { onFoodItemClick(food.id) }, // Clickable behavior
+        shape = RoundedCornerShape(4.dp), // Rounded corner shape for the item
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary), // Border stroke for the item
+        color = Color(0xFFF9CF93) // Background color for the item
     ) {
+        // Row for arranging content horizontally
         Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
+            verticalAlignment = Alignment.CenterVertically, // Align content vertically
+            horizontalArrangement = Arrangement.SpaceBetween // Space between content
         ) {
+            // Text displaying the name of the food
             Text(
-                text = food.name,
-                modifier = Modifier.weight(1f),
-                textAlign = TextAlign.Center,
-                color = MaterialTheme.colorScheme.onTertiaryContainer,
-                style = MaterialTheme.typography.titleLarge
+                text = food.name, // Name of the food
+                modifier = Modifier.weight(1f), // Weight for layout
+                textAlign = TextAlign.Center, // Text alignment
+                color = MaterialTheme.colorScheme.onTertiaryContainer, // Text color
+                style = MaterialTheme.typography.titleLarge // Typography style
             )
+
+            // AsyncImage for displaying the image of the food
             AsyncImage(
-                modifier = Modifier.weight(1f),
-                model = food.imageUrl,
-                contentDescription = null,
-                contentScale = ContentScale.FillBounds
+                modifier = Modifier.weight(1f), // Weight for layout
+                model = food.imageUrl, // Image URL
+                contentDescription = null, // Description for accessibility
+                contentScale = ContentScale.FillBounds // Scale type for content
             )
         }
     }
